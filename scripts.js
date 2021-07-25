64c38d0af50a960b6c13df562a19d4b0da68ba28
ken <ken.mbira@student.moringaschool.com>
2021-07-25 17:00:10 +0300

    modify the portfolio section to have hover effects on the images

--- scripts.js
+++ scripts.js
@@ -13,4 +13,55 @@ $(document).ready(function() {
         $("#program-showing").toggle();
         $("#program-hidden").toggle();
     });
+    $("#black-project").hover(function() {
+            $("#black-text").show();
+        },
+        function() {
+            $("#black-text").hide();
+        })
+
+    $("#security-project").hover(function() {
+            $("#security-text").show();
+        },
+        function() {
+            $("#security-text").hide();
+        })
+    $("#abstract-project").hover(function() {
+            $("#abstract-text").show();
+        },
+        function() {
+            $("#abstract-text").hide();
+        })
+    $("#status-project").hover(function() {
+            $("#status-text").show();
+        },
+        function() {
+            $("#status-text").hide();
+        })
+    $("#health-project").hover(function() {
+            $("#health-text").show();
+        },
+        function() {
+            $("#health-text").hide();
+        })
+    $("#button-project").hover(function() {
+            $("#button-text").show();
+        },
+        function() {
+            $("#button-text").hide();
+        })
+    $("#logo-project").hover(function() {
+            $("#logo-text").show();
+        },
+        function() {
+            $("#logo-text").hide();
+        })
+    $("#restaurant-project").hover(function() {
+            $("#restaurant-text").show();
+        },
+        function() {
+            $("#restaurant-text").hide();
+        })
+
+
 })
